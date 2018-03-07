18e94d685c3a4f25688d4ceabd2a53cbac31142f
mdekauwe <mdekauwe@gmail.com>
2018-03-07 21:22:40 +1100

    vectorised A

--- dalec_EnKF.py
+++ dalec_EnKF.py
@@ -60,8 +60,11 @@ def main(fname):
 
 def dump_output(c, A):
 
-    x = np.sum(A[c.POS_GPP,:])
-    x2 = np.sum(A[c.POS_GPP,:]**2)
+    #x = np.sum(A[c.POS_GPP,:])
+    #x2 = np.sum(A[c.POS_GPP,:]**2)
+
+    x = np.sum(A[c.POS_CL,:])
+    x2 = np.sum(A[c.POS_CL,:]**2)
 
     ensemble_member_avg = x / float(c.nrens)
     ensemble_member_stdev_error = np.sqrt((x2 - \
@@ -146,30 +149,29 @@ def setup_initial_conditions(p, c):
 
 def initialise_ensemble(p, c, A):
 
-    for j in range(c.nrens):
-        A[c.POS_RA,j] = 1.0 + np.random.normal(0.0, 0.1 * 1.0)
-        A[c.POS_AF,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_AW,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_AR,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_LF,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_LW,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_LR,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_RH1,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_RH2,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_D,j] = 0.3 + np.random.normal(0.0, 0.1 * 0.3)
-        A[c.POS_GPP,j] = 1.0 + np.random.normal(0.0, 0.1 * 1.0)
-        A[c.POS_CF,j] = p.cf0 + np.random.normal(0.0, 0.1 * p.cf0)
-        A[c.POS_CW,j] = p.cw0 + np.random.normal(0.0, 0.1 * p.cw0)
-        A[c.POS_CR,j] = p.cr0 + np.random.normal(0.0, 0.1 * p.cr0)
-        A[c.POS_CL,j] = p.cl0 + np.random.normal(0.0, 0.1 * p.cl0)
-        A[c.POS_CS,j] = p.cs0 + np.random.normal(0.0, 0.1 * p.cs0)
+    A[c.POS_RA,:] = 1.0 + np.random.normal(0.0, 0.1 * 1.0, c.nrens)
+    A[c.POS_AF,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_AW,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_AR,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_LF,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_LW,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_LR,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_RH1,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_RH2,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_D,:] = 0.3 + np.random.normal(0.0, 0.1 * 0.3, c.nrens)
+    A[c.POS_GPP,:] = 1.0 + np.random.normal(0.0, 0.1 * 1.0, c.nrens)
+    A[c.POS_CF,:] = p.cf0 + np.random.normal(0.0, 0.1 * p.cf0, c.nrens)
+    A[c.POS_CW,:] = p.cw0 + np.random.normal(0.0, 0.1 * p.cw0, c.nrens)
+    A[c.POS_CR,:] = p.cr0 + np.random.normal(0.0, 0.1 * p.cr0, c.nrens)
+    A[c.POS_CL,:] = p.cl0 + np.random.normal(0.0, 0.1 * p.cl0, c.nrens)
+    A[c.POS_CS,:] = p.cs0 + np.random.normal(0.0, 0.1 * p.cs0, c.nrens)
 
     return A
 
 def initialise_error_covariance(c, Q):
 
     Q = np.random.normal(0.0, 1.0, c.ndims*c.nrens).reshape(c.ndims, c.nrens)
-    
+
     return Q
 
 def setup_stochastic_model_error(p):
